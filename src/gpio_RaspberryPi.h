7796bd42b36ac7be37e954c9504ddb56c981c034
Johan Simonsson <github@fun-tech.se>
2014-12-06 16:03:44 +0100

    Raspberry PI gpio header

---
+++ src/gpio_RaspberryPi.h
@@ -0,0 +1,27 @@
+#ifndef  __GPIO_RASPBERRYPI_H
+#define  __GPIO_RASPBERRYPI_H
+
+/**
+ * The main gpio header on the Raspberry PI, rev2
+ */
+enum GPIO_Name {
+    GPIO_Pin03 =  2,
+    GPIO_Pin05 =  3,
+    GPIO_Pin07 =  4,
+    GPIO_Pin08 = 14,
+    GPIO_Pin10 = 15,
+    GPIO_Pin11 = 17,
+    GPIO_Pin12 = 18,
+    GPIO_Pin13 = 27,
+    GPIO_Pin15 = 22,
+    GPIO_Pin16 = 23,
+    GPIO_Pin18 = 24,
+    GPIO_Pin19 = 10,
+    GPIO_Pin21 =  9,
+    GPIO_Pin22 = 25,
+    GPIO_Pin23 = 11,
+    GPIO_Pin24 =  8,
+    GPIO_Pin26 =  7
+};
+
+#endif  // __GPIO_RASPBERRYPI_H
