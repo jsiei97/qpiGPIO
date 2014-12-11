a44919328d3237f335e207ee5227f9fc46133678
Johan Simonsson <github@fun-tech.se>
2014-12-11 06:36:56 +0000

    Make it more clean what is a log

--- src/DebugDefines.h
+++ src/DebugDefines.h
@@ -2,7 +2,7 @@
 #define  __DEBUGDEFINES_H
 
 
-#define myOut() qDebug() << __PRETTY_FUNCTION__ << __LINE__
-#define myErr() qDebug() << __PRETTY_FUNCTION__ << __LINE__ << "Error:"
+#define myOut() qDebug() << "[" << __PRETTY_FUNCTION__ << __LINE__ << "]"
+#define myErr() qDebug() << "[" << __PRETTY_FUNCTION__ << __LINE__ << "]" << "Error:"
 
 #endif  // __DEBUGDEFINES_H 
