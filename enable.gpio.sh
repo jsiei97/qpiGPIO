dacd461ad2acc6340ca7f00af540bf80b66242f5
Johan Simonsson <github@fun-tech.se>
2015-11-17 21:56:19 +0100

    Quickfix to enable and fix gpio perm

---
+++ enable.gpio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# User right bug in Raspbian, 
+# this script is a quickfix for now...
+
+echo 17 > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio17/direction 
+chown :gpio /sys/class/gpio/gpio17/value 
+chmod g+w /sys/class/gpio/gpio17/value 
+
